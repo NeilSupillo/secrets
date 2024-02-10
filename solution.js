6cb9744c48e17075fb8a1efaa43ee3b2f102821f
Neil Supillo <neilsupillo@gmail.com>
2024-02-10 15:39:25 +0800

    bootstrap

--- solution.js
+++ solution.js
@@ -26,8 +26,8 @@ app.use(
 );
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.static("public"));
-app.use(express.static(__dirname + "public"));
-app.set("views", __dirname + "views");
+app.use(express.static(__dirname + "/public"));
+app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
 app.use(passport.initialize());
