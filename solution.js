7a10fc4673fe69bf54198e0b0ce6c24994598c4e
Neil Supillo <neilsupillo@gmail.com>
2024-02-10 11:13:30 +0800

    add /

--- solution.js
+++ solution.js
@@ -26,7 +26,7 @@ app.use(
 );
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.static("public"));
-app.use(express.static(__dirname + "public"));
+app.use(express.static(__dirname + "/public"));
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
