884039b3e17f3679cd46921356c3b2817a6e027c
Neil Supillo <neilsupillo@gmail.com>
2024-02-10 21:00:22 +0800

    gggg

--- solution.js
+++ solution.js
@@ -26,13 +26,13 @@ app.use(
 );
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// app.use(express.static(__dirname + "public"));
-// app.set("views", __dirname + "views");
+// app.use(express.static("public"));
+// app.set("views", "views");
 // app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + "public"));
+app.set("views", __dirname + "views");
+app.set("view engine", "ejs");
 
 app.use(passport.initialize());
 app.use(passport.session());
